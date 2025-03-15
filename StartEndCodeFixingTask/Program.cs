7fa581cfe1c3c57342ed9192c7542c3cdf3a49d5
21xchen <144434186+21xchen@users.noreply.github.com>
2025-03-15 17:59:20 +0000

    Update Program.cs

--- StartEndCodeFixingTask/Program.cs
+++ StartEndCodeFixingTask/Program.cs
@@ -9,6 +9,25 @@
             // Show the test outputs for the test table in the Readme
             // Put your evidence where it says Do the coding task in C#
 
+            bool validChoice = false;
+    		do
+    		{
+        		int difference = -1;
+        		Console.WriteLine("enter a start year");
+        		int startYear = Convert.ToInt16(Console.ReadLine());
+        		Console.WriteLine("enter an end year");
+        		int endYear = Convert.ToInt16(Console.ReadLine());
+    		if (startYear >= endYear)
+    		{
+    			Console.WriteLine("start year must be before 2000");
+    		}
+    		else
+    		{
+    			validChoice = true;
+    			difference = endYear - startYear;
+    			Console.WriteLine(difference);
+    		}
+    		} while (validChoice == false);
 
         }
     }
